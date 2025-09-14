84ceaa83014b9e5a77a28c7c4f65ef7a5b0dc3eb
Abhay-0103 <ab0321054@gmail.com>
2025-09-14 18:59:18 +0530

    Added Welcome message

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -6,17 +6,39 @@ export const sendVerificationEmail = async (email, verificationToken) => {
 
     try {
         const response = await mailtrapClient.send({
-            from:sender,
+            from: sender,
             to: recipient,
-            subject:"Verify Your Email",
+            subject: "Verify Your Email",
             html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
             category: "Email Verification"
         });
 
         console.log("Email sent successfully", response);
-	} catch (error) {
-		console.error(`Error sending verification`, error);
+    } catch (error) {
+        console.error(`Error sending verification`, error);
 
-		throw new Error(`Error sending verification email: ${error}`);
-	}
+        throw new Error(`Error sending verification email: ${error}`);
+    }
 };
+
+
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{ email }];
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: "62ac1ca5-44eb-4d3f-8d72-44ccd8bb004d",
+            template_variables: {
+                "name": "Lucky 😉"
+            },
+        });
+
+        console.log("Welcome email sent successfully", response);
+    } catch (error) {
+        console.log(`Error sending welcome email`, error);
+
+        throw new Error(`Error sending welcome email: $(error)`);
+    }
+}
