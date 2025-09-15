8b955897aebeac6bd6da9e4a84b137719d1c8781
Abhay-0103 <ab0321054@gmail.com>
2025-09-15 22:24:22 +0530

    added routes and floating animation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom"; // no BrowserRouter here
+import FloatingShape from "./components/FloatingShape";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+      <FloatingShape
+        color="bg-green-500"
+        size="w-64 h-64"
+        top="-5%"
+        left="10%"
+        delay={0}
+      />
+      <FloatingShape
+        color="bg-green-500"
+        size="w-48 h-48"
+        top="-5%"
+        left="80%"
+        delay={5}
+      />
+      <FloatingShape
+        color="bg-green-500"
+        size="w-32 h-32"
+        top="-5%"
+        left="-10%"
+        delay={2}
+      />
+
+      <Routes>
+        <Route path="/" element={<h1 className="text-white text-4xl">Home</h1>} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
