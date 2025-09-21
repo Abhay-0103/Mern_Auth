2483c98e306d2a0e5b87940a1789b0a1e051be5d
Abhay-0103 <ab0321054@gmail.com>
2025-09-21 15:36:48 +0530

    Added Loading Spinner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,34 +4,48 @@ import FloatingShape from "./components/FloatingShape";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import EmailVerificationPage from "./pages/EmailVerificationPage";
+import DashboardPage from "./pages/DashboardPage";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authStore";
 import { useEffect } from "react";
 
+
 // protected routhes that required authentication
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
 
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
 
-// redirect authenticated user to the home page
-const RedirectAuthenticatedUser = ({children}) => {
-  const {isAuthenticated, user} = useAuthStore();
-
-  if(isAuthenticated && user.isVerified){
-    return <Navigate to="/" replace />
+  if (!user.isVerified) {
+    return <Navigate to="/verify-email" replace />;
   }
 
   return children;
-}
+};
+
+// redirect authenticated user to the home page
+const RedirectAuthenticatedUser = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if (isAuthenticated && user.isVerified) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
 
 function App() {
-  const { isCheckingAuth, checkAuth, isAuthenticated, user }= useAuthStore()
+  const { isCheckingAuth, checkAuth, isAuthenticated, user } = useAuthStore();
 
-useEffect(() => {
-  checkAuth()
-},[checkAuth])
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
 
-console.log("isAuthenticated", isAuthenticated)
-console.log("User",user)
+if (isCheckingAuth) return <LoadingSpinner />;
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
@@ -58,13 +72,25 @@ console.log("User",user)
       />
 
       <Routes>
-        <Route path="/" element={"Home"} />
-        <Route path="/signup" element={<RedirectAuthenticatedUser>
-          <SignUpPage />
-        </RedirectAuthenticatedUser>} />
-        <Route path="/login" element={<RedirectAuthenticatedUser>
-          <LoginPage />
-        </RedirectAuthenticatedUser>} />
+        <Route path="/" element={<ProtectedRoute>
+          <DashboardPage />
+        </ProtectedRoute>} />
+        <Route
+          path="/signup"
+          element={
+            <RedirectAuthenticatedUser>
+              <SignUpPage />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RedirectAuthenticatedUser>
+              <LoginPage />
+            </RedirectAuthenticatedUser>
+          }
+        />
         <Route path="/verify-email" element={<EmailVerificationPage />} />
       </Routes>
       <Toaster />
